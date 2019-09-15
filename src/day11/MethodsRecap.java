bca95e2994bf12c2326dcb4de3506199496fb63b
Tech Lead Academy <techleadacademy@Techs-iMac.local>
2019-09-15 12:07:29 -0400

    before luch

--- src/day11/MethodsRecap.java
+++ src/day11/MethodsRecap.java
@@ -3,8 +3,26 @@ package day11;
 public class MethodsRecap {
 	
 	public static void main(String[] args) {
-        sayHello();
-        sayWord("Mute");
+//        sayHello();
+//        sayWord("Mute");
+ //       printNumbers(6,8);
+ //       printNumbersTernary(11, 20);
+//          String str = getNumbers(12,11);
+//          System.out.println(str);
+//		  int result = getMax(6,3,29);
+//		  System.out.println(result);
+		
+		// Lunch till 1 PM
+		
+		String str = getTwoFirstLetters("hello");
+		System.out.println(str);
+		
+		System.out.println(getTwoFirstLetters("banana"));
+		System.out.println(getTwoFirstLetters("Sunday"));
+		
+		sayHello();
+
+		  
 	}
 	
 	public static void sayHello() {
@@ -19,9 +37,64 @@ public class MethodsRecap {
 	
 	// if i is greater then j -> print i first then j
 	// if j is greater then i -> print j first then i
+	//                                  6      8
 	public static void printNumbers(int i, int j) {
-		
+		if(i > j) {
+			System.out.println(i +" "+j);
+		}else {
+			System.out.println(j +" "+i);
+		}
 	}
+	
+	
+	// if num1 is greater then num2 -> print num1 first then num2
+	// if num2 is greater then num2 -> print num2 first then num1
+	// with ternary
+	public static void printNumbersTernary(int num1, int num2) {
+       String str = num1 > num2 ? num1+" "+num2 : num2+" "+num1;
+       System.out.println(str);
+	}
+	
+	
+	// method name is getNumbers 
+	// return type is String
+	// if i is greater then j -> return i first then j
+	// if j is greater then i -> return j first then i
+	
+	public  static String getNumbers(int i, int j) {
+		String output;
+		if(i > j) {
+			output = i+" "+j;
+		}else {
+			output = j+" "+i;
+	    }
+		return output;
+	}
+	
+	
+	public static int getMax(int num1, int num2, int num3) {
+		int  max = 0;
+		if(num1 > num2 && num1 > num3) {
+			max = num1;
+		}else if(num2 > num1 && num2 > num3) {
+			max = num2;
+		}else {
+			max = num3;
+		}
+	
+		return max;
+	}
+	
+	
+	// create a method which returns String
+	// method accepts one String argument
+	// return first two letters of string
+	public static String getTwoFirstLetters(String str) {
+		
+		String firstTwoLetter = str.substring(0,2);
+		return firstTwoLetter;
+	}
+	
 	
 
 }
